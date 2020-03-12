1bef0c3a3e27f4023656d4d243747258601f2b25
ulif <uli@gnufix.de>
2020-03-12 17:11:03 +0100

    Create project skeleton.

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+tests_require = [
+    'pytest',
+]
+
+
+setup(
+        name="pgp-milter",
+        version="0.1.dev0",
+        description="Mail filter for automatic PGP-encryption of messages.",
+        long_description=read(
+            'README.rst') + '\n\n\n' + read('CHANGES.rst') + '\n\n\n',
+        long_description_content_type='text/x-rst',
+        url="https://github.com/ulif/pgp-milter",
+        author="ulif",
+        author_email="uli@gnufix.de",
+        license="GPL3",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Console",
+            "Intended Audience :: System Administrators",
+            "Topic :: Security :: Cryptography",
+            (
+                "License :: OSI Approved :: "
+                "GNU General Public License v3 or later (GPLv3+)"),
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: Implementation :: CPython",
+        ],
+        packages=["pgp_milter"],
+        extras_require={
+            'test': tests_require,
+        },
+        tests_require=tests_require,
+        zip_safe=False,
+)
