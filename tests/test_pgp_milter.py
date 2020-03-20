8ca58829d8788c5035a87407b93984ddc94d18f1
ulif <uli@gnufix.de>
2020-03-20 10:43:08 +0100

    More tests.

--- tests/test_pgp_milter.py
+++ tests/test_pgp_milter.py
@@ -1,6 +1,8 @@
 import pgp_milter
 import pkg_resources
-from pgp_milter import handle_options
+from pgp_milter import (
+    __version__, handle_options
+)
 
 
 def test_importable():
@@ -10,9 +12,16 @@ def test_importable():
 
 def test_version():
     # assure versions in setup.py and package match
-    v1 = pgp_milter.__version__
+    v1 = __version__
     v2 = pkg_resources.get_distribution('pgp_milter').version
     assert v1 == v2
+
+
+def test_handle_options_defaults():
+    # we can expect sensible defaults set
+    args = handle_options([])
+    assert args.version is False
+
 
 def test_handle_options_version():
     # we support `--version'
