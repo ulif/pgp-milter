e3759a92d2b83849a24c8d4228087975579f7565
ulif <uli@gnufix.de>
2020-07-08 15:52:30 +0200

    Test socket option default.

--- tests/test_pgp_milter.py
+++ tests/test_pgp_milter.py
@@ -35,6 +35,7 @@ def test_handle_options_defaults():
     args = handle_options([])
     assert args.version is False
     assert args.debug is False
+    assert args.socket == "inet6:30072@[::1]"
 
 
 def test_handle_options_debug():
