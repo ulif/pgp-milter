6ae1940748afaf6777dac4d66907926b39a4ca72
ulif <uli@gnufix.de>
2020-08-18 17:31:56 +0200

    In tests use really working sample values.

--- tests/test_pgp_milter.py
+++ tests/test_pgp_milter.py
@@ -69,9 +69,9 @@ def test_run(monkeypatch):
     def mock_runmilter(name, sock, timeout=300):
         Milter._mock_vals = [name, sock, timeout]
     monkeypatch.setattr("Milter.runmilter", mock_runmilter)
-    result = run_main("testmilter", "inet6:[::1]")
+    result = run_main("testmilter", "inet6:2323@[::1]")
     assert Milter.factory == PGPMilter
-    assert Milter._mock_vals == ["testmilter", "inet6:[::1]", 300]
+    assert Milter._mock_vals == ["testmilter", "inet6:2323@[::1]", 300]
     assert result is None
 
 
