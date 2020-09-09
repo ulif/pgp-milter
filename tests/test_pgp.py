85a19583d19e11d6c3a5dd64bb00ccf25535b46e
ulif <uli@gnufix.de>
2020-09-09 12:55:01 +0200

    flake8.

--- tests/test_pgp.py
+++ tests/test_pgp.py
@@ -12,9 +12,9 @@ from pgp_milter import pgp
 
 
 # PGP fingerprints
-FPR_ALICE  = "FC576D66A075141F41770B15F028476ACE63FE41"
+FPR_ALICE = "FC576D66A075141F41770B15F028476ACE63FE41"
 FPR_ALICE2 = "BC8E0FFE80B27CAB91D6D2315B1D44F70BA91072"
-FPR_BOB    = "FDBE48E6FE58D021A5C8BE3B982AD46FA8789D5C"
+FPR_BOB = "FDBE48E6FE58D021A5C8BE3B982AD46FA8789D5C"
 
 
 def replace_pgp_msg(text):
@@ -165,4 +165,3 @@ def test_get_fingerprints_overlapping_names(tmpdir):
     gpg.import_keys(open("tests/alice2.pub", "r").read())
     assert [FPR_ALICE] == pgp.get_fingerprints(gpg, "alice@sample.net")
     assert [FPR_ALICE2] == pgp.get_fingerprints(gpg, "thealice@sample.net")
-
