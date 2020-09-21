ca5a7620035e2dd6a5343df2e051d3c79fbc7ec4
ulif <uli@gnufix.de>
2020-09-21 13:19:35 +0200

    Ensure, that config file paths are absolute.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,15 @@
 from pgp_milter.config import (
     OPTIONS_DEFAULTS,
+    config_paths,
     )
 
 
 def test_defaults_exist():
     # there is a set of defaults available
     assert OPTIONS_DEFAULTS is not None
+
+
+def test_config_paths_are_absolute():
+    # no relative paths, please
+    paths = config_paths()
+    assert [True, True, True] == [x.is_absolute() for x in paths]
