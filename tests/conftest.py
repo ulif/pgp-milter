66215104c1489c3b7a7d2631298a27162a9ac608
ulif <uli@gnufix.de>
2020-10-01 16:25:25 +0200

    flake8.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ def home_dir(request, monkeypatch, tmpdir):
     tmpdir.mkdir("home")
     monkeypatch.setenv("HOME", str(tmpdir / "home"))
     os.chdir(str(tmpdir / "home"))
+
     def teardown():
         os.chdir(_old_cwd)
     request.addfinalizer(teardown)
