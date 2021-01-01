9da1840312063e7d78d57d065ba7024366431dbd
ulif <uli@gnufix.de>
2021-01-01 16:35:03 +0100

    Rename config var: buflen -> bufsize.

--- tests/test_config.py
+++ tests/test_config.py
@@ -28,7 +28,7 @@ def test_get_config_dict(home_dir, monkeypatch):
     result = get_config_dict()
     assert result == {
         'debug': True, 'socket': 'bar', 'timeout': 300,
-        'buflen': 8192, 'pgphome': '~/.pgphome'
+        'bufsize': 8192, 'pgphome': '~/.pgphome'
     }
 
 
