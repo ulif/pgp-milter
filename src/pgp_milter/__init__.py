2bc03fae1646bb0eb0f95c76766f92748c900278
ulif <uli@gnufix.de>
2021-07-19 04:23:12 +0200

    Do not encrypt already encrypted messages.

--- src/pgp_milter/__init__.py
+++ src/pgp_milter/__init__.py
@@ -6,7 +6,7 @@ from email import message_from_binary_file
 from email.policy import default as default_policy
 from io import BytesIO
 from pgp_milter.config import get_config_dict
-from pgp_milter.pgp import encrypt_msg, KeyManager
+from pgp_milter.pgp import msg_encrypted, encrypt_msg, KeyManager
 
 
 __version__ = "0.1.dev0"  # set also in setup.py
@@ -145,6 +145,8 @@ class PGPMilter(Milter.Base):
                 "X-PGPMilter", "Scanned by PGPMilter %s" % __version__, -1)
         self.fp.seek(0)
         msg = message_from_binary_file(self.fp, policy=default_policy)
+        if msg_encrypted(msg):  # msg already encrypted?
+            return Milter.ACCEPT
         changed, new_msg = encrypt_msg(msg, self.rcpts, self.key_mgr)
         if not changed:
             return Milter.ACCEPT
