c5154aec8a8e7ce0460c9a4cd8c55882739c975a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 03:38:10 +0000

    Adding tarfile member sanitization to extractall()

--- prep_reuse.py
+++ prep_reuse.py
@@ -12,5 +12,24 @@ with tarfile.open(src_path) as tar:
     # remove packaging artefacts
     members = [m for m in members if not m.name.endswith("PKG-INFO")]
     members = [m for m in members if ".egg-info" not in m.name]
-    tar.extractall(".", members)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, ".", members)
 os.rename(os.listdir()[0], "pgp-milter")
